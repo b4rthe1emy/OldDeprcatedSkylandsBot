5fe465bcfea6a0b046d33e59fb1bbb8ab520504f
b4rthe1emy <barth.villo@gmail.com>
2024-02-17 12:34:40 +0100

    disabled on_member_join

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import discord
 from discord.ext import commands
 from datetime import datetime
 
-import member_join
+# import member_join
 import polls
+import private_messages
 
 intents: discord.Intents = discord.Intents.default()
 intents.message_content = True
@@ -24,8 +25,9 @@ intents: discord.Intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="/", intents=intents)
 
 
-polls.setup(bot)
 # member_join.setup(client)
+polls.setup(bot)
+# private_messages.setup(bot)
 
 
 @bot.event
